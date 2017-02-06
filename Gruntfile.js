e60573dc1b519471dd37df61d9b4f8f47c9f042c
LuckyKarin <kuangyi0601@gmail.com>
2017-02-06 11:36:20 +0800

    修改Gruntfile配置，增加clean等操作

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,10 +16,30 @@ module.exports = function(grunt) {
       all: [
         'Gruntfile.js',
         'tasks/*.js',
-        'tasks/lib/*.js'
+        'tasks/lib/*.js',
+        'test/src/scripts/{,*/}*.js'
       ],
       options: {
         jshintrc: '.jshintrc'
+      }
+    },
+
+    clean: {
+      dest: ['test/dest']
+    },
+
+    filerev: {
+      options: {
+        algorithm: 'md5',
+        length: 8
+      },
+      scripts: {
+        src: 'test/src/scripts/{,*/}*.js',
+        dest: 'test/dest/scripts'
+      },
+      styles: {
+        src: 'test/src/styles/{,*/}*.css',
+        dest: 'test/dest/styles'
       }
     },
 
@@ -28,32 +48,40 @@ module.exports = function(grunt) {
       scripts: {
         options: {
           params: {
-            baseUrl: "//s6.rr.itc.cn/h5/ucenter/v4/master/js/",
-            mark: "just test!"
+            baseUrl: "//domain.com/h5/ucenter/v4/master/js/",
+            mark: "test js mark adding!"
           },
           hasCommitLog: true,
-          process: function(releaseLogObj, key) {
-            var fragmentMap = {
-              "test-a.js": "碎片a",
-              "test-b.js": "碎片b"
-            };
-            if(fragmentMap[key]) {
-              releaseLogObj[key]["fragment"] = fragmentMap[key];
-            }
+          process: function(key) {
+              var fragmentMap = {
+                "lib.js": "碎片a",
+                "main.js": "碎片b"
+              };
+              return {
+                  fragment: fragmentMap[key]
+              };
           }
         },
-        src: "test/scripts/*.js",
+        src: "test/dest/scripts/*.js",
+        dest: "release.log"
+      },
+      styles: {
+        options: {
+          params: {
+            baseUrl: "//domain.com/h5/ucenter/v4/master/css/"
+          }
+        },
+        src: "test/dest/styles/*.css",
         dest: "release.log"
       }
-    },
+    }
 
   });
 
   grunt.loadTasks('tasks');
-
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  require('load-grunt-tasks')(grunt);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'releaselog']);
+  grunt.registerTask('default', ['jshint', 'clean', 'filerev', 'releaselog']);
 
 };
